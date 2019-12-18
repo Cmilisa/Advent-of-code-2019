d432b97e21f526a2d00666edadf2fecfb5b3e07d
Cmilisa <57148042+Cmilisa@users.noreply.github.com>
2019-12-18 14:09:58 +0100

    Add files via upload

--- day7/thrusters.py
+++ day7/thrusters.py
@@ -1,113 +1,118 @@
+import itertools
+
 f = open("program.txt", "r")
 
 #codes = f.readline().split(",")
 codes = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,\
 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0".split(",")
+codes = [int(i) for i in codes]
 
-param_nb = {'1' : 3, '2' : 3, '9' : 0, '3' : 1, '4' :  1, '5' : 2, '6' : 2, '7' : 3, '8' : 3}
+params = {'01' : 'rrw', '02' : 'rrw', '03' : 'w', '04' : 'r',
+          '05' : 'rr', '06' : 'rr', '07' : 'rrw', '08' : 'rrw',
+          '99' : ''}
 
 def run(input_list):
     output = 0
     rpi = 0
     while True:
-        #print("rpi : ", rpi)
-        inst = codes[rpi]
-        inst = '0' * (param_nb[inst[-1]] + 2 - len(inst)) + inst
+        inst = str(codes[rpi])
+        opcode = ('0' + inst)[-2:]
+        opsize = len(params[opcode])
+        inst = '0' * (opsize + 2 - len(inst)) + inst
+        modes = list(inst[:-2])[::-1]
 
-        if inst[-1] == '1' or inst[-1] == '2':
-            arg1 = 0
-            arg2 = 0
-            target = 0
-            if inst[2] == '1':
-                arg1 = int(codes[rpi + 1])
-            elif inst[2] == '0':
-                arg1 = int(codes[int(codes[rpi + 1])])
+        if opcode == '99':
+            #print("[*] Program halted")
+            break
 
-            if inst[1] == '1':
-                arg2 = int(codes[rpi + 2])
-            elif inst[1] == '0':
-                arg2 = int(codes[int(codes[rpi + 2])])
+        args = []
+        
+        for i in range(0, opsize):
+            if params[opcode][i] == 'r':
+                if modes[i] == '1':
+                    args.append(codes[rpi + 1 + i ])
+                elif modes[i] == '0':
+                    args.append(codes[codes[rpi + 1 + i]])
+            elif params[opcode][i] == 'w':
+                args.append(codes[rpi + 1 + i])
 
-            target = int(codes[rpi + 3])
+        """print("rpi : ", rpi)
+        print("code : ", codes[rpi:rpi+6])
+        print("opcode : ", opcode)
+        print("r/w : ", params[opcode])
+        print("modes : ", modes)
+        print("args : ", args)
+        input(">")"""
+
+
+        if opcode == '01':
+            codes[args[2]] = args[0] + args[1]
+            rpi += opsize + 1
+
+        elif opcode == '02':
+            codes[args[2]] = args[0] * args[1]
+            rpi += opsize + 1
             
-            if inst[-1] == '1':
-                codes[target] = str(arg1 + arg2)
-                rpi += param_nb['1'] + 1
-            elif inst[-1] == '2':
-                codes[target] = str(arg1 * arg2)
-                rpi += param_nb['2'] + 1
-            
-
-        elif inst[-1] == '3':
-            target = int(codes[rpi + 1])
-
+        elif opcode == '03':
             if input_list == []:
                 inp = input("[*] Program has requested an input : ")
             else:
                 inp = input_list[0]
                 input_list.remove(inp)
                 
-            codes[target] = str(inp)
-            rpi += param_nb['3'] + 1
+            codes[args[0]] = int(inp)
+            rpi += opsize + 1
 
-        elif inst[-1] == '4':
-            arg1 = 0
+        elif opcode == '04':
+            #print("[*] Output", args[0])
+            output = args[0]
+            rpi += opsize + 1
 
-            if inst[0] == '1':
-                arg1 = int(codes[rpi + 1])
-            elif inst[0] == '0':
-                arg1 = int(codes[int(codes[rpi + 1])])
-
-            print("[*] Output", arg1)
-            output = arg1
-            rpi += param_nb['4'] + 1
-
-        elif inst[-1] == '5' or inst[-1] == '6':
-            arg1 = 0
-
-            if inst[1] == '1':
-                arg1 = int(codes[rpi + 1])
-            elif inst[1] == '0':
-                arg1 = int(codes[int(codes[rpi + 1])])
-
-            if (arg1 != 0 and inst[-1] == '5') or (arg1 == 0 and inst[-1] == '6'):
-                target = int(codes[rpi + 2])
-                rpi = target
+        elif opcode == '05':
+            if args[0] != 0:
+                rpi = args[1]
             else:
-                rpi += param_nb['5'] + 1
+                rpi += opsize + 1
 
-
-        elif inst[-1] == '7' or inst[-1] == '8':
-            arg1 = 0
-            arg2 = 0
-
-            if inst[2] == '1':
-                arg1 = int(codes[rpi + 1])
-            elif inst[2] == '0':
-                arg1 = int(codes[int(codes[rpi + 1])])
-
-            if inst[1] == '1':
-                arg2 = int(codes[rpi + 2])
-            elif inst[1] == '0':
-                arg2 = int(codes[int(codes[rpi + 2])])
-
-            target = int(codes[rpi + 3])
-
-            if (arg1 < arg2 and inst[-1] == '7') or (arg1 == arg2 and inst[-1] == '8'):
-                codes[target] = 1
+        elif opcode == '06':
+            if args[0] == 0:
+                rpi = args[1]
             else:
-                codes[target] = 0
+                rpi += opsize + 1
 
-            rpi += param_nb['7'] + 1
+        elif opcode == '07':
+            if args[0] < args[1]:
+                codes[args[2]] = 1
+            else:
+                codes[args[2]] = 0
 
+            rpi += opsize + 1
 
-        elif inst[-1] == '9':
-            print("[*] Program halted")
-            break
+        elif opcode == '08':
+            if args[0] == args[1]:
+                codes[args[2]] = 1
+            else:
+                codes[args[2]] = 0
+
+            rpi += opsize + 1
 
         else:
             print("[*] Debug output, wrong opcode : ", inst)
 
     return output
 
-print(run([]))
+
+max_amp = 0
+max_phases = []
+
+for phases in itertools.permutations([0,1,2,3,4]):
+    amplitude = 0
+    for p in phases:
+        amplitude = run([p, amplitude])
+
+    if amplitude > max_amp:
+        max_amp = amplitude
+        max_phases = phases
+    
+print(max_amp)
+print(max_phases)
